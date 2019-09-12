f88db6610932376851c58b187c854d0c068b13c2
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-13 00:30:05 +0300

    Updates

---
+++ precache-manifest.ddcf447dca2ae21aa0e30a9d71dbe579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0f7344180efe88d9f908ba22967c40",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "45fc3e7a3b28a5c9fbad",
+    "url": "/react_tabs/static/css/main.1421b244.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "45fc3e7a3b28a5c9fbad",
+    "url": "/react_tabs/static/js/main.8271c8e9.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
